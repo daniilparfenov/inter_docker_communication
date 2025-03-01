cdd9054597bae036d1e18a88fafc8b5562407b80
vadim <144333283+vadyukkha@users.noreply.github.com>
2025-03-01 17:35:18 +0300

    #add comments

--- producer-consumer/src/consumer.cpp
+++ producer-consumer/src/consumer.cpp
@@ -6,17 +6,24 @@
 #include <iostream>
 
 int main() {
+    // Создание цикла событий libev
     struct ev_loop *evloop = EV_DEFAULT;
 
+    // Создание соединения с RabbitMQ
     AMQP::Address address("amqp://user52:pwd52@host.docker.internal:5672/");
     AMQP::LibEvHandler handler(evloop);
     AMQP::TcpConnection connection(&handler, address);
     AMQP::TcpChannel channel(&connection);
 
+    // Создание очереди и "подписка" на получение сообщений
     channel.declareQueue("task_queue", AMQP::durable)
         .onSuccess([&channel](const std::string &name, uint32_t, uint32_t) {
             channel.consume(name, AMQP::noack)
                 .onReceived([](const AMQP::Message &message, uint64_t, bool) {
+                    /*
+                     * Обработка полученного сообщения
+                     * В данном случае просто выводим его содержимое в консоль
+                     */
                     std::cout << "Получено сообщение из очереди: "
                               << std::string(message.body(), message.body() + message.bodySize())
                               << std::endl;
@@ -26,6 +33,7 @@ int main() {
             std::cout << "Ошибка при получении сообщения: " << message << std::endl;
         });
 
+    // Запуск цикла событий libev
     ev_run(evloop, 0);
     return 0;
 }
