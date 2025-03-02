691ce737adf5bd6919f357fd962feb965f102e20
vadim <144333283+vadyukkha@users.noreply.github.com>
2025-03-02 18:19:53 +0300

    #add comments

--- producer-consumer/src/consumer.cpp
+++ producer-consumer/src/consumer.cpp
@@ -8,16 +8,25 @@
 #include <vector>
 
 int main() {
+    // Создание цикла событий libev
     struct ev_loop *evloop = EV_DEFAULT;
+
+    // Создание соединения с RabbitMQ
     AMQP::Address address("amqp://user52:pwd52@host.docker.internal:5672/");
     AMQP::LibEvHandler handler(evloop);
     AMQP::TcpConnection connection(&handler, address);
     AMQP::TcpChannel channel(&connection);
 
+    // Получение изображения из очереди
     channel.declareQueue("image_queue", AMQP::durable)
         .onSuccess([&channel](const std::string &name, uint32_t, uint32_t) {
             channel.consume(name, AMQP::noack)
                 .onReceived([&channel](const AMQP::Message &message, uint64_t, bool) {
+                    /*
+                     * Обработка полученного изображения
+                     * В данном случае полученное изображение инвертируется
+                     * и отправляется обратно в очередь
+                     */
                     std::vector<uchar> img_data(message.body(),
                                                 message.body() + message.bodySize());
                     cv::Mat img = cv::imdecode(img_data, cv::IMREAD_COLOR);
@@ -27,12 +36,13 @@ int main() {
                         return;
                     }
 
-                    cv::Mat inverted;
-                    cv::bitwise_not(img, inverted);
+                    cv::Mat inverted;                // Инвертированное изображение
+                    cv::bitwise_not(img, inverted);  // Инвертирование изображения
 
-                    std::vector<uchar> encoded_img;
-                    cv::imencode(".jpg", inverted, encoded_img);
+                    std::vector<uchar> encoded_img;               // Кодированное изображение
+                    cv::imencode(".jpg", inverted, encoded_img);  // Кодирование изображения
 
+                    // Отправка обработанного изображения в очередь
                     channel.publish("", "processed_image_queue",
                                     reinterpret_cast<const char *>(encoded_img.data()),
                                     encoded_img.size());
@@ -43,6 +53,7 @@ int main() {
             std::cerr << "Ошибка при получении: " << message << std::endl;
         });
 
+    // Запуск цикла событий libev
     ev_run(evloop, 0);
     return 0;
 }
--- producer-consumer/src/producer.cpp
+++ producer-consumer/src/producer.cpp
@@ -9,12 +9,16 @@
 #include <vector>
 
 int main() {
+    // Создание цикла событий libev
     struct ev_loop *evloop = EV_DEFAULT;
+
+    // Создание соединения с RabbitMQ
     AMQP::Address address("amqp://user52:pwd52@host.docker.internal:5672/");
     AMQP::LibEvHandler handler(evloop);
     AMQP::TcpConnection connection(&handler, address);
     AMQP::TcpChannel channel(&connection);
 
+    // Отправка изображения в очередь
     std::string path = RESOURCES_DIR "/cute_dog.jpg";
     std::vector<uchar> img_data;
 
@@ -26,20 +30,31 @@ int main() {
 
     cv::imencode(".jpg", image, img_data);
 
+    // Создание очереди и отправка сообщений
     channel.declareQueue("image_queue", AMQP::durable)
-        .onSuccess([&connection, &channel, &img_data](const std::string &name, uint32_t, uint32_t) {
+        .onSuccess([&channel, &img_data](const std::string &name, uint32_t, uint32_t) {
+            /*
+             * Отправка сообщений в очередь
+             * В данном случае отправляется 1 изображение
+             */
             channel.publish("", name, reinterpret_cast<const char *>(img_data.data()),
                             img_data.size());
             std::cout << "Изображение отправлено в очередь: " << name << std::endl;
-            // connection.close();
         })
         .onError(
             [](const char *message) { std::cerr << "Ошибка отправки: " << message << std::endl; });
 
+    // Получение обработанного изображения
     channel.declareQueue("processed_image_queue", AMQP::durable)
-        .onSuccess([&channel](const std::string &name, uint32_t, uint32_t) {
+        .onSuccess([&connection, &channel](const std::string &name, uint32_t, uint32_t) {
             channel.consume(name, AMQP::noack)
-                .onReceived([](const AMQP::Message &message, uint64_t, bool) {
+                .onReceived([&connection](const AMQP::Message &message, uint64_t, bool) {
+                    /*
+                     * Обработка полученного изображения
+                     * В данном случае полученное изображение сохраняется в файл output.jpg
+                     */
+
+                    // Декодирование изображения
                     std::vector<uchar> img_data(message.body(),
                                                 message.body() + message.bodySize());
                     cv::Mat img = cv::imdecode(img_data, cv::IMREAD_COLOR);
@@ -48,14 +63,18 @@ int main() {
                                   << std::endl;
                         return;
                     }
+
+                    // Сохранение изображения
                     cv::imwrite("output.jpg", img);
                     std::cout << "Обработанное изображение сохранено как output.jpg" << std::endl;
+                    connection.close();  // Закрытие соединения
                 });
         })
         .onError([](const char *message) {
             std::cerr << "Ошибка при получении обработанного изображения: " << message << std::endl;
         });
 
+    // Запуск цикла событий libev
     ev_run(evloop, 0);
     return 0;
 }
