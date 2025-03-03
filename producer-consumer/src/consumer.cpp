1ff4947c877f467da5c3811b03d413f9215fab27
danya <reactive722@gmail.com>
2025-03-03 13:37:53 +0300

    #update producer to send multiple images; #fix one consumer took all images from queue; #add new input images :)

--- producer-consumer/src/consumer.cpp
+++ producer-consumer/src/consumer.cpp
@@ -17,37 +17,42 @@ int main() {
     AMQP::TcpConnection connection(&handler, address);
     AMQP::TcpChannel channel(&connection);
 
+    // Ограничение количества сообщений, которые потребитель получает за раз
+    channel.setQos(1);  // Гарантирует, что один consumer не возьмет след. сообщение перед тем как
+                        // не подтвердит обработку предыдущего
+
     // Получение изображения из очереди
     channel.declareQueue("image_queue", AMQP::durable)
         .onSuccess([&channel](const std::string &name, uint32_t, uint32_t) {
-            channel.consume(name, AMQP::noack)
-                .onReceived([&channel](const AMQP::Message &message, uint64_t, bool) {
-                    /*
-                     * Обработка полученного изображения
-                     * В данном случае полученное изображение инвертируется
-                     * и отправляется обратно в очередь
-                     */
-                    std::vector<uchar> img_data(message.body(),
-                                                message.body() + message.bodySize());
-                    cv::Mat img = cv::imdecode(img_data, cv::IMREAD_COLOR);
+            channel.consume(name).onReceived([&channel](const AMQP::Message &message,
+                                                        uint64_t deliveryTag, bool) {
+                /*
+                 * Обработка полученного изображения
+                 * В данном случае полученное изображение инвертируется
+                 * и отправляется обратно в очередь
+                 */
+                std::vector<uchar> img_data(message.body(), message.body() + message.bodySize());
+                cv::Mat img = cv::imdecode(img_data, cv::IMREAD_COLOR);
 
-                    if (img.empty()) {
-                        std::cerr << "Ошибка: не удалось декодировать изображение" << std::endl;
-                        return;
-                    }
+                if (img.empty()) {
+                    std::cerr << "Ошибка: не удалось декодировать изображение" << std::endl;
+                    return;
+                }
 
-                    cv::Mat inverted;                // Инвертированное изображение
-                    cv::bitwise_not(img, inverted);  // Инвертирование изображения
+                cv::Mat inverted;                // Инвертированное изображение
+                cv::bitwise_not(img, inverted);  // Инвертирование изображения
 
-                    std::vector<uchar> encoded_img;               // Кодированное изображение
-                    cv::imencode(".jpg", inverted, encoded_img);  // Кодирование изображения
+                std::vector<uchar> encoded_img;               // Кодированное изображение
+                cv::imencode(".jpg", inverted, encoded_img);  // Кодирование изображения
 
-                    // Отправка обработанного изображения в очередь
-                    channel.publish("", "processed_image_queue",
-                                    reinterpret_cast<const char *>(encoded_img.data()),
-                                    encoded_img.size());
-                    std::cout << "Изображение обработано и отправлено обратно." << std::endl;
-                });
+                // Отправка обработанного изображения в очередь
+                channel.publish("", "processed_image_queue",
+                                reinterpret_cast<const char *>(encoded_img.data()),
+                                encoded_img.size());
+                std::cout << "Изображение обработано и отправлено обратно." << std::endl;
+                // Подтверждение обработки сообщения
+                channel.ack(deliveryTag);
+            });
         })
         .onError([](const char *message) {
             std::cerr << "Ошибка при получении: " << message << std::endl;
